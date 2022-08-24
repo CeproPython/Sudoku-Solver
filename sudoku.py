d1e939590d4b256d7b661c6e83dab4295fc8878c
SynnK <93355393+synnkfps@users.noreply.github.com>
2022-08-24 12:00:20 -0300

    sistema de células 1/2
    feito com o chromebook ahushuahsua

--- sudoku.py
+++ sudoku.py
@@ -1,44 +1,43 @@
 import random
 
+
 def main():
     # Tabela do Sudoku #
-    tabela0 = [0,0,0,  0,0,0,  0,0,0]
-    tabela1 = [0,0,0,  0,0,0,  0,0,0]
-    tabela2 = [0,0,0,  0,0,0,  0,0,0]
+    tabela0 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    tabela1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    tabela2 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-    tabela3 = [0,0,0,  0,0,0,  0,0,0]
-    tabela4 = [0,0,0,  0,0,0,  0,0,0]
-    tabela5 = [0,0,0,  0,0,0,  0,0,0]
+    tabela3 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    tabela4 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    tabela5 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-    tabela6 = [0,0,0,  0,0,0,  0,0,0]
-    tabela7 = [0,0,0,  0,0,0,  0,0,0]
-    tabela8 = [0,0,0,  0,0,0,  0,0,0]
+    tabela6 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    tabela7 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    tabela8 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-        # tabela geral #
+    # tabela geral #
     tabela = [tabela0, tabela1, tabela2, tabela3, tabela4, tabela5, tabela6, tabela7, tabela8]
 
     # colocar um numero em um lugar aleatorio #
-    numero_inicial = random.randrange(0,9)
+    numero_inicial = random.randrange(0, 9)
     posicao_inicial = random.choice(tabela)
-    posicao_inicial[random.randrange(0,9)] =  numero_inicial
-
+    posicao_inicial[random.randrange(0, 9)] = numero_inicial
 
     # Gerar a tabela inicial
     # Checar uma tabela aleatoria
     # Colocar um numero aleatorio na tabela (antes verificar se ele já existe)
     # Se ele não existir, ter uma chance de colocar ele, (entre 1 e 10)
 
-    dificuldades = ['facil', 'medio', 'dificil']
-    dificuldade = 'dificil'
+    # dificuldades = ['facil', 'medio', 'dificil']
+    dificuldade = 'facil'
 
     def gerar_chance():
-        match dificuldade:
-            case 'facil':
-                return random.randrange(0, 3)
-            case 'medio':
-                return random.randrange(0, 7)
-            case 'dificil':
-                return random.randrange(0, 10)
+        if dificuldade == 'facil':
+            return random.randrange(0, 3)
+        if dificuldade == 'medio':
+            return random.randrange(0, 7)
+        if dificuldade == 'dificil':
+            return random.randrange(0, 10)
 
     # Gerar tabela checando por duplicados na horizontal
     for tabela_atual in tabela:
@@ -50,7 +49,7 @@ def main():
 
                 # checar se o numero gerado já está na tabela atual
                 if numero_aleatorio in tabela_atual:
-                    pass 
+                    pass
                 else:
                     tabela_atual[random.randrange(0, 9)] = numero_aleatorio
 
@@ -75,7 +74,6 @@ def main():
             primeiros_string += str(i[n])
         else:
             primeiros.append(primeiros_string)
-            primeiros_string = ''
 
     for casa_a_casa in range(9):
         checar_primeiros(casa_a_casa)
@@ -92,7 +90,7 @@ def main():
         else:
             if tem:
                 main()
-            return tem 
+            return tem
 
     contador = 0
     for i in primeiros:
@@ -100,7 +98,27 @@ def main():
         if tem_duplicados(i):
             print(f'Tabela {contador} tem duplicados!')
 
+    celulas = [
+        [tabela[0][0:3], tabela[1][0:3], tabela[2][0:3]],
+        [tabela[0][3:6], tabela[1][3:6], tabela[2][3:6]],
+        [tabela[0][6:9], tabela[1][6:9], tabela[2][6:9]],
+
+        [tabela[3][0:3], tabela[4][0:3], tabela[5][0:3]],
+        [tabela[3][3:6], tabela[4][3:6], tabela[5][3:6]],
+        [tabela[3][6:9], tabela[4][6:9], tabela[5][6:9]],
+
+        [tabela[6][0:3], tabela[7][0:3], tabela[8][0:3]],
+        [tabela[6][3:6], tabela[7][3:6], tabela[8][3:6]],
+        [tabela[6][6:9], tabela[7][6:9], tabela[8][6:9]],
+    ]
+
+    print(celulas)
+
+
 main()
+# while True:
+#    main()
+
 #while True:
 #    main()
 
